cb2960cf8df71b3a19c8fd3148b6836511ac7475
Olena Morozova <appsapps530@gmail.com>
2025-02-21 19:47:50 +0200

    hw8, part-1

--- js/task-3.js
+++
@@ -1,8 +0,0 @@
-const nameInput = document.querySelector("#name-input");
-const output = document.querySelector("#name-output");
-
-nameInput.addEventListener("input", (event) => {
-  const trimmedName = event.currentTarget.value.trim();
-  
-  output.textContent = trimmedName !== "" ? trimmedName : "Anonymous";
-});
